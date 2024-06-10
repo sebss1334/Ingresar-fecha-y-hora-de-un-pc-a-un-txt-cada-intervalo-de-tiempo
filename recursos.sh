59e4bbf41b4d31d8519a25a2a088564a99e83875
sebss1334 <sebasgonzalezleon7@gmail.com>
2024-06-10 15:22:05 -0500

    Add files via upload

---
+++ recursos.sh
@@ -0,0 +1,13 @@
+N=5
+archivo="recursos.csv"
+
+echo "Fecha,Hora,Memoria (MB),CPU (%)" > "$archivo"
+
+while true; do
+	FECHA_HORA=$(date +"%Y-%m-%d,%H:%M:%S")
+	MEMORIA=$(free -m | awk '/Mem:/ {print $3}')
+	CPU=$(top -bn1 | awk '/%Cpu/ {print $2}')
+
+	echo "$FECHA_HORA,$MEMORIA,$CPU" >> "$archivo"
+	sleep "$N"
+done &
